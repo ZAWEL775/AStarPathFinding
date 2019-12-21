da8d73107c0df206707bbd083416e590056f81a5
Zach Welch <Jaybo5@hotmail.com>
2019-12-21 17:18:01 -0600

    'master'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+/*Zachary Welch*/
+import React, { Component } from 'react';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+class App extends Component {
+    render(){
+        return (
+        <div className="App">
+            <PathfindingVisualizer></PathfindingVisualizer>>
+        </div>
+        );
+    }
+}
+export default App;
